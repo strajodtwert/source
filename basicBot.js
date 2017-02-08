633bdc87017f2673f468d03eede2b8fd01e1b222
strajodtwert <strajosrb95@gmail.com>
2017-02-08 12:28:31 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -239,7 +239,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/strajodtwert/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/strajodtwert/source/master/lang/srb.json",
         chat: null,
@@ -250,7 +250,7 @@
             botName: "BalkanMusicBOT",
             language: "serbian",
             chatLink: "https://rawgit.com/strajodtwert/source/master/lang/srb.json",
-            scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+            scriptLink: "https://rawgit.com/strajodtwert/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
