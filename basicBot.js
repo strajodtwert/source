18cec2fc730baac7dec185a7bf2859ceef1663ed
strajodtwert <strajosrb95@gmail.com>
2017-02-08 13:34:25 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -98,7 +98,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/basicBot/source/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/strajodtwert/source/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
