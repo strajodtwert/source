85936dab51d88e67674e35f1d27fd30716ee25db
strajodtwert <strajosrb95@gmail.com>
2017-03-03 16:37:40 +0100

    Update basicBot3.js

--- basicBot3.js
+++ basicBot3.js
@@ -3093,7 +3093,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me This bot was created by ' + botCreator + ', but is now maintained by ' + botMaintainer + ".");
+                        API.sendChat('/me Bot je napravljen od strance ' + botCreator + ', ali sada ga odrzava ' + botMaintainer + ".");
                     }
                 }
             },
