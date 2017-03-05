185f350f6dcd4b72c65c11f90c0c6f2241a57847
strajodtwert <strajosrb95@gmail.com>
2017-03-05 14:29:06 +0100

    Update balkanmusicbotproba.js

--- balkanmusicbotproba.js
+++ balkanmusicbotproba.js
@@ -1956,6 +1956,42 @@
                     }
                 }
             },
+            
+            cookieCommand: {
+                command: 'cookie',
+                rank: 'user',
+                type: 'startsWith',
+                getCookie: function (chat) {
+                    var t = Math.floor(Math.random() * basicBot.chat.truth.length);
+                    return basicBot.chat.truth[t];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.truth, {nameto: user.username, namefrom: chat.un, truth: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
 
             cycleCommand: {
                 command: 'cycle',
