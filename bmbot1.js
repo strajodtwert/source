f633fea226afcf55408f6ada1f8de883b39aa220
strajodtwert <strajosrb95@gmail.com>
2017-06-13 22:04:42 +0200

    Create bmbot1.js

--- bmbot1.js
+++ bmbot1.js
@@ -3793,10 +3793,10 @@
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!bmbot1.commands.executable(this.rank, chat)) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        if (typeof bmbot1.settings.discord === "string")
-                            API.sendChat(subChat(bmbot1.chat.discord, {link: bmbot1.settings.discord}));
+                        if (typeof basicBot.settings.discord === "string")
+                            API.sendChat(subChat(basicBot.chat.discord, {link: basicBot.settings.discord}));
                     }
                 }
             },
