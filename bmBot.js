43737984e311d4d9d490ab25b56324bde9f06550
strajodtwert <strajosrb95@gmail.com>
2017-06-13 22:08:31 +0200

    Create bmBot.js

--- bmBot.js
+++ bmBot.js
@@ -3785,6 +3785,20 @@
                     }
                 }
             },
+		
+            discordCommand: {
+                command: 'discord',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        if (typeof basicBot.settings.discord === "string")
+                            API.sendChat(subChat(basicBot.chat.discord, {link: basicBot.settings.discord}));
+                    }
+                }
+            },
 
             youtubeCommand: {
                 command: 'youtube',
